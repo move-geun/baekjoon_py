07e7cc2fe160a6ab6cf83d598e711a237e375911
이동근 <ehdrms1426@gmail.com>
2022-09-30 20:03:28 +0900

    누적합

---
+++ 누적합/11659_구간합구하기4/s1.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+prefix_sum = [0]
+ 
+tmp = 0 
+for i in nums:
+    tmp += i
+    prefix_sum.append(tmp)
+
+for _ in range(M):
+    i,j = map(int, input().split())
+    print(prefix_sum[j]-prefix_sum[i-1])
+
