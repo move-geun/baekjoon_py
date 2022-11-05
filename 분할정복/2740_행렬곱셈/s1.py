c401a4695f5811ec507ebc68789cffbb9fc8651d
이동근 <ehdrms1426@gmail.com>
2022-11-05 23:52:44 +0900

    2740_행렬곱셈

--- 분할정복/2740_행렬곱셈/s1.py
+++ 분할정복/2740_행렬곱셈/s1.py
@@ -1,3 +1,9 @@
 import sys
 
-N = int(sys.stdin.readline())
+N,M = map(int, sys.stdin.readline().split())
+A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+M,K = map(int, sys.stdin.readline().split())
+B = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+
+print(A)
+print(B)
