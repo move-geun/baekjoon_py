e04de8c33dc68899364ee36101f78dbe20388190
이동근 <ehdrms1426@gmail.com>
2022-11-06 18:06:07 +0900

    2740_행렬곱셈

--- 분할정복/2740_행렬곱셈/s1.py
+++ 분할정복/2740_행렬곱셈/s1.py
@@ -5,5 +5,15 @@ A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 M,K = map(int, sys.stdin.readline().split())
 B = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
 
-print(A)
-print(B)
+C = [[0 for _ in range(K)] for _ in range(N)]
+
+for n in range(N):
+    for k in range(K):
+        for m in range(M):
+            C[n][k] += A[n][m] * B[m][k]
+
+
+for i in C:
+    for j in i:
+        print(j, end = ' ')
+    print()
