dd85c3237d1bf518310d1ff6e12080c0ec59938c
move-geun <85543481+move-geun@users.noreply.github.com>
2023-05-13 23:37:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.37 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,21 @@
-from collections import defaultdict
+import collections
+
 
 def solution(participant, completion):
-    answer = ''
-    finish = defaultdict(int)
-    for i in participant:
-        finish[i] += 1
-    for i in completion:
-        finish[i] -= 1
-    for i in finish:
-        if finish[i] >= 1:
-            answer += i
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
+
+# from collections import defaultdict
+
+# def solution(participant, completion):
+#     answer = ''
+#     finish = defaultdict(int)
+#     for i in participant:
+#         finish[i] += 1
+#     for i in completion:
+#         finish[i] -= 1
+#     for i in finish:
+#         if finish[i] >= 1:
+#             answer += i
         
-    return answer
+#     return answer
