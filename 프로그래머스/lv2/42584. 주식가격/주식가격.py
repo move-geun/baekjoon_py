4428a9c820124e30a7ba8e4bf0430c8a28e49114
move-geun <85543481+move-geun@users.noreply.github.com>
2023-05-17 18:56:32 +0900

    [level 2] Title: 주식가격, Time: 37.88 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,12 +1,14 @@
+from collections import deque
 def solution(prices):
     answer = []
-    for i in range(len(prices) - 1):
-        sec = 0
-        for j in range(i, len(prices) - 1):
-            if(prices[i] <= prices[j]):
-                sec += 1
-            else:
+    que=deque(prices)  # queue 선언
+    while(que):
+        price=que.popleft() ## pop --> list길이가 점점 짧아지면서 for문 효율 증가
+        cnt=0 # cnt 초기화
+        for com_price in que : 
+            cnt+=1  ## 다음에바로 떨어지더라도 1초 후라는 조건이므로 비교하자마자 증가해도됨
+            if price > com_price : # 가격이 떨어지자마자 break 치고 나오면 맞음
                 break
-        answer.append(sec);
-    answer.append(0);
+        answer.append(cnt) # 증가된거 list에 저장하고 다음 pop으로 이동
+
     return answer
