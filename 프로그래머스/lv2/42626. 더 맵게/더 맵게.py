e8e2775a89169b0796c9a89c461d97cf14c61dd1
move-geun <85543481+move-geun@users.noreply.github.com>
2023-05-18 23:19:08 +0900

    [level 2] Title: 더 맵게, Time: 2017.00 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,19 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    if scoville[0] >= K:
+        return answer
+    
+    while scoville[0] < K:
+        if len(scoville) == 1:
+            return -1
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a+b*2)
+        answer += 1
+        
+    return answer
+
