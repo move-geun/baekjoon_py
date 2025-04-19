9d432f1c8e31b321c0c3ec0fdbac91d8c9ad81f1
move-geun <85543481+move-geun@users.noreply.github.com>
2025-04-19 21:53:33 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,10 @@
+n = int(input())
+lst = list(map(str, input()))
+points = [i for i in range(1,27)]
+
+tmp = 0
+for i in range(len(lst)):
+    idx = ord(lst[i])-97
+    tmp += points[idx]*(31**i)
+
+print(tmp)
