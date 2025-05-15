aa7502ec898a666919626356655cad84568fca06
move-geun <85543481+move-geun@users.noreply.github.com>
2025-05-15 14:28:07 +0900

    [Silver II] Title: 과일 탕후루, Time: 232 ms, Memory: 36936 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -4,27 +4,24 @@ from collections import defaultdict
 input = sys.stdin.readline
 
 n = int(input())
+lst = list(map(int, input().split(' ')))
 
-fruits = list(map(int, input().split()))
-
-cnt = defaultdict(int)
-distinct = 0            
-si = 0
+kinds = 0
 maxv = 0
+fruits = defaultdict(int)
+si = 0
 
 for ei in range(n):
-    if cnt[fruits[ei]] == 0:
-        distinct += 1
-    cnt[fruits[ei]] += 1
+    if fruits[lst[ei]] == 0:
+        kinds += 1
+    fruits[lst[ei]] += 1
 
-    while distinct > 2:
-        cnt[fruits[si]] -= 1
-        if cnt[fruits[si]] == 0:
-            distinct -= 1
+    while kinds > 2:
+        fruits[lst[si]] -= 1
+        if fruits[lst[si]] == 0:
+            kinds -= 1
         si += 1
-
-    length = ei - si + 1
-    if length > maxv:
-        maxv = length
+    
+    maxv = max(maxv, ei-si+1)
 
 print(maxv)
