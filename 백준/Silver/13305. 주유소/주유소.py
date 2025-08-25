f3d14685b3e84b23247cab8e396ada285ea99db8
move-geun <85543481+move-geun@users.noreply.github.com>
2025-08-25 23:47:38 +0900

    [Silver III] Title: 주유소, Time: 164 ms, Memory: 130444 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+dist = list(map(int, sys.stdin.readline().split()))
+cost = list(map(int, sys.stdin.readline().split()))
+min_cost = cost[0]
+aws = 0
+for i in range(N-1):
+    if cost[i] < min_cost:
+        min_cost = cost[i]
+    aws += min_cost * dist[i]
+
+print(aws)
