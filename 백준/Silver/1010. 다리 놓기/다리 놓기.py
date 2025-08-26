95f18994e878d01570bed7ee34bfadc49c0251b4
move-geun <85543481+move-geun@users.noreply.github.com>
2025-08-26 23:54:14 +0900

    [Silver V] Title: 다리 놓기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+input = stdin.readline
+
+T = int(input())
+
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+for _ in range(T):
+    n,m = map(int, input().split())
+    print(factorial(m) // (factorial(n)*factorial(m-n)))
